85d401a0beda2cc007d6c7d078002a5b8502138d
Mostafa2132 <12m0stafa7@gmail.com>
2026-06-03 22:56:59 +0300

    frist commit

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,16 +2,17 @@ import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { ProductDetailsClient } from "./productDetailsClient";
 
-export default function ProductDetailsPage({
+export default async function ProductDetailsPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
+  const resolvedParams = await params;
   return (
     <div className="min-h-full">
       <SiteHeader />
       <main className="mx-auto w-full max-w-6xl px-4 pb-10 pt-8">
-        <ProductDetailsClient slug={params.slug} />
+        <ProductDetailsClient slug={resolvedParams.slug} />
       </main>
       <SiteFooter />
     </div>
